72248cd83604683f1ee8d2e3014dff8433bd9813
zergamat <mateusz.liszewski@gmail.com>
2022-12-13 10:38:38 +0100

    comments

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,53 +1,52 @@
-/*
-Funkcja DEBUG_PRINT daje na porcie szerogowym zdefiniowanym jako Serial np.
-Serial.begin(115200);
-na wyjściu dane (po kolei):
-millis: funkcja_w_której_ją_przywołano nazwa_pliku_z_programem: linia_w_pliku podana_zmienna_w_funkcji
-*/
-
 #include <Arduino.h>
 
-// Aby DebugUtils.h działał, należy odkomentowac linie #define DEBUG_UTILS i/lub DEBUG_UTILS1
-#define DEBUG_UTILS
-#define DEBUG_UTILS1
+#define DEBUG_UTILS  // uncomment to enable primary debugging (DS and DL)
+#define DEBUG_UTILS1 // uncomment to enable secoundary debugging (DS1 and DL1)
 #include <DebugUtils.h>
 
-int zmienna = 0;
+int Counter = 0;
 
-// zapowiedz funkcji
 void test();
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("\n\nESP8266_TEST_Biblioteka - START");
-  // SP i SL są skrótami Serial.print i Serial.println (z lenistwa).
-  // są dostępne zawsze w tej bibliotece:
-  SP("SP - Odpowiednik Serial.print() Zawsze dostępny w tej bibliotece");
+  Serial.println("\n\nbasic.cpp - START");
+  // SP() works as Serial.print() - always available
+  // SL() works an Serial.println() - always available
+  SP("SP - substitute of Serial.print() - allways available");
   SL();
-  SL("SL - Odpowiednik Serial.println() Zawsze dostępny w tej bibliotece");
+  SL("SL - substitute of Serial.println() - allways available");
 
-  DS("DS - Odpowiednik Serial.print() Wymaga #define DEBUG_UTILS");
+  DS("DS - substitute of Serial.print() - requires #define DEBUG_UTILS");
   DL();
-  DL("DL - Odpowiednik Serial.println() Wymaga #define DEBUG_UTILS");
+  DL("DL - substitute of Serial.println() - requires #define DEBUG_UTILS");
 
-  DS1("DS1 - Odpowiednik Serial.print() Wymaga #define DEBUG_UTILS1. Kolejny poziom debugowania.");
+  DS1("DS1 - substitute of Serial.print() - requires #define DEBUG_UTILS1. Intended to be secound level of debugging.");
   DL1();
-  DL1("DL1 - Odpowiednik Serial.println() Wymaga #define DEBUG_UTILS1. Kolejny poziom debugowania.");
+  DL1("DL1 - substitute of Serial.println() - requires #define DEBUG_UTILS1. Intended to be secound level of debugging.");
 }
 
 void loop()
 {
   test();
-  zmienna++;
+  Counter++;
 
-  delay(30000);
+  delay(5000);
 }
 
 void test()
 {
-  DF("raport");
-  DF(zmienna);
-  DF1("raport1");
-  DF1(zmienna);
+  /*
+  Function DF and DF1 prints on serial monitor:
+  actual millis: function_in_witch_DF_was_called name_of_the_file_it_was_called: line_it_was_called data_given_as_DF_variable
+  for example:
+  DF("example");
+  could give sth like this:
+  5000: void test() basic.cpp:44 example
+  */
+  DF("report");
+  DF(Counter);
+  DF1("report1");
+  DF1(Counter);
 }
